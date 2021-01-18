efc68593cc042862fe94d88293d26708fea3f32e
i-kostenevich <igor.kostenevich557@gmail.com>
2021-01-18 04:22:24 +0200

    completed toogle text

--- vue.js
+++ vue.js
@@ -6,17 +6,18 @@ const App = {
   data() {
     return {
       btnNameNext: 'Вперед',
-      isPresseddBtnEnd: false,
-      activeIndex: 0, 
+      isPressedBtnEnd: false,
+      activeIndex: 0,
       steps: [
         {title: 'Основы', text: 'В блоке вы познакомитесь со всеми основами Vue.js на практике. На протяжении блока мы напишем реактивное приложение, в процессе разработки которого разберем вся базу фреймворка.'},
         {title: 'Компоненты', text: 'Один из самых важных блоков в курсе, где вы узнаете все о компонентах. В блоке мы напишем 2 разных приложения и создадим более 5 различных UI компонентов как в реальной разработке. Блок расскажет про абсолютно все составляющие, которые есть в компонентах: взаимодействие, slots, асинхронные и динамические компоненты и тонна примеров.'},
         {title: 'Роутер', text: 'В данном блоке вы узнаете все о том, как работает мультиязычность во Vue. Мы создадим миниклон Gmail в данном блоке, где вы на практике увидите как работать с динамическим роутером.'},
         {title: 'Vuex', text: 'В блоке вы узнаете абсолютно все про Vuex. Вы узнаете как работать с данными, какие есть лучшие практики по их программированию и структурированию. Все на практике.'},
         {title: 'Composition', text: 'Одним из наиболее важных обновлений в Vue 3 является появление альтернативного синтаксиса Composition API. В этом блоке вы узнаете все, чтобы полностью пользоваться данными синтаксисом на практических примерах. Помимо этого вы узнаете как работать совместно с Vue Router и Vuex.'}
-      ]
+      ],
     }
   },
+  
   methods: {
     prev() {
       if (this.activeIndex > 0) {
@@ -25,38 +26,33 @@ const App = {
     },
     reset() {
       this.activeIndex = 0
-      this.isPresseddBtnEnd = false
+      this.isPressedBtnEnd = false
     },
     nextOfFinish() {
       if(this.activeIndex < this.steps.length - 1){
         this.activeIndex++
       }
       if(this.btnNameNext === 'Закончить'){
-        console.log('ww22ww');
-        this.isPresseddBtnEnd= true
+        this.isPressedBtnEnd = true
       }
     },
     setActive(idx) {
-      this.activeIndex = idx
-    },
-    showText(step){
-      // this.steps[item].text
-      console.log(step.text);
-      // return step.text
-      // step.text
-      // console.log(this.steps[idx].text);
+      if(this.isPressedBtnEnd == false) {
+        this.activeIndex = idx
+      }
     }
   },
+
   computed: {
     isDisabledButton() {
       return !this.activeIndex
     },
-    endStep() {
-      return this.activeIndex >= this.steps.length - 1 ? this.btnNameNext = 'Закончить' : this.btnNameNext = 'Вперед'
+    isFinishStep() {
+      return this.activeIndex === this.steps.length - 1 ? this.btnNameNext = 'Закончить' : this.btnNameNext = 'Вперед'
+    },
+    activeStepText() {
+      return this.steps[this.activeIndex].text;
     }
-    // тут стоит определить несколько свойств:
-    // 1. текущий выбранный шаг
-    // 3. находимся ли мы на последнем шаге
   }
 }
 
